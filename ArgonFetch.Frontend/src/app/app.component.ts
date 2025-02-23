e9ca4d2477b491f2fdd4ef6bcea060722fcafa46
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-02-23 19:35:21 +0100

    refactor: remove unused app module and component styles, update theme toggle icon

--- ArgonFetch.Frontend/src/app/app.component.ts
+++ ArgonFetch.Frontend/src/app/app.component.ts
@@ -3,18 +3,20 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [RouterModule, CommonModule, FontAwesomeModule]
 })
 export class AppComponent {
   faSun = faSun;
   faMoon = faMoon;
+  faGithub = faGithub;
   isDarkTheme$;
 
   constructor(private themeService: ThemeService) {
